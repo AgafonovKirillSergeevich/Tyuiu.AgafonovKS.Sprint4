e7acfef98bc8aa846f36b0006b2458721bc36386
Кирилл Агафонов <Kirya_agafonov_2002@mail.ru>
2023-11-04 16:59:34 +0500

    Task3

---
+++ Tyuiu.AgafonovKS.Sprint4.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AgafonovKS.Sprint4.Task3.V3.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint4.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { 9, 4, 5, 4, 8 },
+                                          { 7, 6, 7, 7, 4 },
+                                          { 4, 4, 3, 5, 6 },
+                                          { 6, 5, 9, 4, 9 },
+                                          { 9, 7, 8, 7, 7 };
+            int res = ds.Calculate(mas2);
+            int wait = 14;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AgafonovKS.Sprint4.Task3.V3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AgafonovKS.Sprint4.Task3.V3.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint4.Task3.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Агафонов К. С. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант 3                                                               *");
+            Console.WriteLine("* Выполнил: Агафонов Кирилл Сергеевич | ИИПб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 3 до 9.                          *");
+            Console.WriteLine("* Найдите сумму элементов в первом столбце массива.                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] myArray = { { 9, 4, 5, 4, 8 }, { 7, 6, 7, 7, 4 }, { 4, 4, 3, 5, 6 }, { 6, 5, 9, 4, 9 }, { 9, 7, 8, 7, 7 } };
+            int rows = myArray.GetUpperBound(0) + 1;
+            int columns = myArray.GetUpperBound(1) + 1;
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(myArray[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.Calculate(myArray);
+            Console.WriteLine("Сумма элементов первого столбца массива = " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
